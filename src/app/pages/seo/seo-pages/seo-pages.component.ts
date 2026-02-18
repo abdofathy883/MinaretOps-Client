930ce64df0e2ba56195d28108cb6a84273f305e7
Abdelrahman Fathy <abdofathy883@gmail.com>
2026-02-18 08:05:35 +0200

    fixed seo implementation

--- src/app/pages/seo/seo-pages/seo-pages.component.ts
+++ src/app/pages/seo/seo-pages/seo-pages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { SeoService } from '../../../services/seo/seo.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ICreateSeo, ISeo } from '../../../model/seo/i-seo';
 
 @Component({
   selector: 'app-seo-pages',
@@ -14,33 +15,61 @@ export class SeoPagesComponent implements OnInit{
   private fb = inject(FormBuilder);
 
   seoForm!: FormGroup;
-  isSubmitting = signal<boolean>(false);
+  selectForm!: FormGroup;
+  // seoContent: ISeo | null = null;
+  isSubmitting = false;
   selectedPage = signal<string>('');
+  private ogImage!: File;
+
+  alertMessage = '';
+  alertType = 'info';
+
 
   ngOnInit(): void {
-    
+    this.initializeSelectForm();
   }
 
-  onPageChange(event: Event) {
-    const selectElement = event.target as HTMLSelectElement;
-    const pageRoute = selectElement.value;
+  // onPageChange(event: Event) {
+  //   const selectElement = event.target as HTMLSelectElement;
+  //   const pageRoute = selectElement.value;
     
-    if (pageRoute) {
-      this.selectedPage.set(pageRoute);
-      this.getSeoContent(pageRoute);
-    } 
+  //   if (pageRoute) {
+  //     this.selectedPage.set(pageRoute);
+  //     this.getSeoContent(pageRoute);
+  //   } 
+  // }
+
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.ogImage = event.target.files[0];
+    }
   }
 
-  getSeoContent(pageRoute: string) {
-    this.seoService.getSeoContent(pageRoute).subscribe({
-      next: (data: any) => {
-        this.initializeForm(data);
+  onSelectPage() {
+    if (this.selectForm.invalid) {
+      return;
+    }
+    this.seoService.getSeoContent(this.selectForm.value.lang, this.selectForm.value.route).subscribe({
+      next: (response) => {
+        this.initializeForm(response);
+        // this.seoContent = response;
       },
-      error: () => {
-        this.initializeForm({});
+      error: (error) => {
+        console.log('Error fetching SEO content:', error);
       }
     });
   }
+
+  // getSeoContent(pageRoute: string) {
+  //   this.seoService.getSeoContent(pageRoute).subscribe({
+  //     next: (data: any) => {
+  //       this.initializeForm(data);
+  //     },
+  //     error: () => {
+  //       this.initializeForm({});
+  //     }
+  //   });
+  // }
 
   initializeForm(data: any){
     this.seoForm = this.fb.group({
@@ -54,11 +83,42 @@ export class SeoPagesComponent implements OnInit{
     });
   }
 
+  initializeSelectForm(){
+    this.selectForm = this.fb.group({
+      route: ['', Validators.required],
+      lang: ['ar', Validators.required]
+    });
+  }
+
   onSubmit(){
-    if (this.seoForm.valid) {
-      this.isSubmitting.set(true);
-      // TODO: Implement save logic
-      setTimeout(() => this.isSubmitting.set(false), 1000);
+    if (this.seoForm.invalid) {
+      return;
     }
+
+    this.isSubmitting = true;
+    const seoData: ICreateSeo = {
+      route: this.selectForm.value.route,
+      language: this.selectForm.value.lang,
+      title: this.seoForm.value.title,
+      description: this.seoForm.value.description,
+      keywords: this.seoForm.value.keywords,
+      ogTitle: this.seoForm.value.ogTitle,
+      ogImage: this.ogImage,
+      canonicalUrl: this.seoForm.value.canonicalUrl,
+      robots: this.seoForm.value.robots
+    }
+    this.seoService.saveNewSeo(seoData).subscribe({
+      next: (response) => {
+        this.isSubmitting = false;
+        this.alertMessage = 'تم حفظ محتوى السيو بنجاح';
+        this.alertType = 'success';
+      },
+      error: (error) => {
+        this.isSubmitting = false;
+        this.alertMessage = 'حدث خطأ أثناء حفظ محتوى السيو';
+        this.alertType = 'danger';
+        console.log('Error saving SEO content:', error);
+      }
+    });
   }
 }
--- src/app/services/seo/seo.service.ts
+++ src/app/services/seo/seo.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../api-service/api.service';
+import { ICreateSeo, ISeo } from '../../model/seo/i-seo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,21 @@ export class SeoService {
   private endpoint: string = 'seo';
   constructor(private api: ApiService) { }
 
-  getSeoContent(pageRoute: string) {
-    // TODO: Implement actual API call
-    return this.api.get(`${this.endpoint}/${pageRoute}`);
+  getSeoContent(lang: string, pageRoute: string): Observable<ISeo> {
+    return this.api.get<ISeo>(`${this.endpoint}/${lang}/${pageRoute}`);
+  }
+
+  saveNewSeo(seoData: ICreateSeo): Observable<ISeo> {
+    const formData = new FormData();
+    formData.append('route', seoData.route);
+    formData.append('language', seoData.language);
+    if (seoData.title) formData.append('title', seoData.title);
+    if (seoData.description) formData.append('description', seoData.description);
+    if (seoData.keywords) formData.append('keywords', seoData.keywords);
+    if (seoData.ogTitle) formData.append('ogTitle', seoData.ogTitle);
+    if (seoData.ogImage) formData.append('ogImage', seoData.ogImage);
+    if (seoData.canonicalUrl) formData.append('canonicalUrl', seoData.canonicalUrl);
+    if (seoData.robots) formData.append('robots', seoData.robots);
+    return this.api.post<ISeo>(this.endpoint, formData);
   }
 }
